2d84a221018f5cebf9470a4a58405754fefdc4e2
Ricardo Fideles <ricardo@ricardofideles.com.br>
2017-11-15 15:52:48 -0200

    app

---
+++ App.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Moment from 'moment';
+
+export default class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {timeNow: this.timeNow()};
+  }
+
+  componentDidMount(){
+    setInterval(() => {
+      this.setState({
+        timeNow: this.timeNow(),
+      });
+    }, 1000);
+  }
+
+  timeNow() {
+    return Moment().format('hh:mm:ss');
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Time Now</Text>
+        <Text style={styles.timeNow}>{this.state.timeNow}</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  timeNow: {
+    textShadowColor: '#0AAFE6',
+    textShadowOffset: {
+      width: 0,
+      height: 0
+    },
+    textShadowRadius: 10,
+    fontSize: 70,
+    color: '#daf6ff'
+  },
+  container: {
+    flex: 1,
+    backgroundColor: '#000',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
